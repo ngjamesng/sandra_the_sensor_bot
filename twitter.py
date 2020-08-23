26bfb4e431677070255eb0c3f80883f98ce540fd
ngjamesng <just.james.ng@gmail.com>
2020-08-23 13:00:54 -0700

    add text access

--- twitter.py
+++ twitter.py
@@ -1,7 +1,6 @@
 from twython import Twython
 from message import Message
 from sensor_data import Sensor
-import json
 
 from auth import (
     consumer_key,
@@ -20,5 +19,4 @@ twitter = Twython(
 m = Message()
 message = m.get_message()
 resp = twitter.update_status(status=message)
-resp_dict = json.loads(resp)
-print("Tweeted: %s" % resp.text)
+print("Tweeted: %s" % resp["text"])
