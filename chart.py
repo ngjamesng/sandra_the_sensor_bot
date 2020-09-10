1d2098f3d5d49e7d98e35ba47e53bde8d1c977a6
ngjamesng <just.james.ng@gmail.com>
2020-09-09 22:03:38 -0700

    fix key dict key names

--- chart.py
+++ chart.py
@@ -12,28 +12,26 @@ class Chart:
         takes in JSON data and creates a chart
         """
         self.json_data = json_data
-        self.cpu_temp = [record['cpu_temp'] for record in self.json_data]
-        self.room_temp = [record['room_temp'] for record in self.json_data]
+        self.temperature = [record['temperature'] for record in self.json_data]
         self.humidity = [record['humidity'] for record in self.json_data]
-        self.air_pressure = [record['air_pressure']
-                             for record in self.json_data]
+        self.pressure = [record['pressure'] for record in self.json_data]
         self.date = [record['date'] for record in self.json_data]
 
     def __repr__(self):
         """show representation of Chart"""
 
-        return f"< Chart
-        room_temp = '{self.room_temp}'
-        humidity = '{self.humidity}'
-        air_pressure = '{self.air_pressure}' >"
+        return f"< Chart temperature = '{self.temperature}' humidity = '{self.humidity}' air_pressure = '{self.pressure}' >"
 
     def plot(self):
         """
         create a plot graph of the sensor data
         """
-        plt.plot(self.date, self.room_temp, label="room temp")
-        plt.plot(self.date, self.cpu_temp, label="cpu temp")
+        plt.plot(self.date, self.temperature, label="room temp")
+        plt.plot(self.date, self.humidity, label="humidity")
         plt.legend()
         plt.xlabel("date/time")
         plt.ylabel("Temperature(F)")
         plt.show()
+
+
+
