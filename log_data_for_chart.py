48115372d6754ab88ef91b6cbb94d4b72cf9d6d1
ngjamesng <just.james.ng@gmail.com>
2020-09-09 20:29:50 -0700

    add date field

--- log_data_for_chart.py
+++ log_data_for_chart.py
@@ -1,23 +1,35 @@
 from sensor_data import Sensor
 from csv import DictWriter
+from datetime import datetime
+
 
 def get_data():
-    """
+    '''
     gets the data from the sensors. 
-    """
+    data = {
+        "temperature": "",
+        "humidity": "",
+        "pressure": ""
+        "date" : ""
+    }
+    '''
 
     sensor = Sensor()
     data = sensor.get_data()
-    temp = data.get("temperature")
-    humidity = data.get("humidity")
-    pressure = data.get("pressure")
-
-    print("temp!", temp)
+    data["date"] = datetime.now().strftime("%d/%m/%Y %H:%M")
+    print("data", data)
+    return data
 
 
 def write_data(data):
     """
     Takes in data and writes the results into a CSV file. 
     """
+    file_name = "data_log.csv"
+    data = get_data()
+    with open(file_name, "W") as csv_file:
+        headers = ["temp", "humidity", "pressure", "date"]
+        csv_writer = DictWriter(csv_file, fieldnames=headers)
+
 
 get_data()
