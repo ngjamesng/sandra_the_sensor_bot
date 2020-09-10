61bfcd53fe85057f8273907e1d81b077c12055d5
ngjamesng <just.james.ng@gmail.com>
2020-09-09 19:23:42 -0700

    add data logger

---
+++ log_data_for_chart.py
@@ -0,0 +1,21 @@
+from sensor_data import Sensor
+from csv import DictWriter
+
+def get_data():
+    """
+    gets the data from the sensors. 
+    """
+
+    sensor = Sensor()
+    data = sensor.get_data()
+    temp = data.temperature
+    humidity = data.humidity
+    pressure = data.pressure
+
+    print("temp!", temp)
+    
+
+def write_data(data):
+    """
+    Takes in data and writes the results into a CSV file. 
+    """
