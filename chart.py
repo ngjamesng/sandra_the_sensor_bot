28759c5cbfa10908c70a353f8014b628a04d3107
ngjamesng <just.james.ng@gmail.com>
2020-09-10 00:15:02 -0700

    format code

--- chart.py
+++ chart.py
@@ -12,7 +12,8 @@ class Chart:
         Opens a CSV file, reads the data and sets the data.
         """
         self.data = data
-        self.temperature = [float(record.get('temperature')) for record in self.data]
+        self.temperature = [float(record.get('temperature'))
+                            for record in self.data]
         self.humidity = [float(record.get('humidity')) for record in self.data]
         self.pressure = [float(record.get('pressure')) for record in self.data]
         self.date = [record.get('date') for record in self.data]
@@ -22,27 +23,26 @@ class Chart:
 
         return f"< Chart temperature = '{self.temperature}' humidity = '{self.humidity}' air_pressure = '{self.pressure}' >"
 
-
     def plot_and_create_image(self, image_name="chart.png"):
         """
         create a plot graph of the sensor data.
         """
-        
+
         fig, temperature_line = plt.subplots()
-        
+
         temperature_color = "tab:red"
         temperature_line.set_xlabel("date & time")
         temperature_line.set_ylabel("Temperature (F)", color=temperature_color)
-        temperature_line.plot(self.date, self.temperature, color=temperature_color)
+        temperature_line.plot(self.date, self.temperature,
+                              color=temperature_color)
         temperature_line.tick_params(axis='y', labelcolor=temperature_color)
-        
+
         humidity_line = temperature_line.twinx()
-        
+
         humidity_color = "tab:blue"
         humidity_line.set_ylabel("humidity (%)", color=humidity_color)
         humidity_line.plot(self.date, self.humidity, color=humidity_color)
         humidity_line.tick_params(axis='y', labelcolor=humidity_color)
-        
-        
+
         fig.tight_layout()
         plt.savefig(image_name)
