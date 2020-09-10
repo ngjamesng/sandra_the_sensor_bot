1d74b83ca9297b4a87f23f2113707042350bcf92
ngjamesng <just.james.ng@gmail.com>
2020-09-09 20:33:59 -0700

    add csvwriter

--- log_data_for_chart.py
+++ log_data_for_chart.py
@@ -7,29 +7,30 @@ def get_data():
     '''
     gets the data from the sensors. 
     data = {
-        "temperature": "",
-        "humidity": "",
-        "pressure": ""
-        "date" : ""
+        "temperature": "73.5 F",
+        "humidity": "50.2 %",
+        "pressure": "1111 mbar"
+        "date" : "dd/mm/YY H:M"
     }
     '''
 
     sensor = Sensor()
     data = sensor.get_data()
     data["date"] = datetime.now().strftime("%d/%m/%Y %H:%M")
-    print("data", data)
     return data
 
 
 def write_data(data):
     """
-    Takes in data and writes the results into a CSV file. 
+    Takes in data and writes the result into a CSV file. 
     """
     file_name = "data_log.csv"
     data = get_data()
     with open(file_name, "W") as csv_file:
-        headers = ["temp", "humidity", "pressure", "date"]
+        headers = ["temperature", "humidity", "pressure", "date"]
         csv_writer = DictWriter(csv_file, fieldnames=headers)
+        csv_writer.writerow(data)
 
 
-get_data()
+data = get_data()
+write_data(data)
