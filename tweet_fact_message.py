d604636716c016ae06ec03a193f3b1eda0e77cdf
James <just.james.ng@gmail.com>
2020-10-19 23:00:31 -0700

    add max char count check

--- tweet_fact_message.py
+++ tweet_fact_message.py
@@ -18,10 +18,28 @@ twitter = Twython(
 
 # get fact for today
 BASE_URL = "http://numbersapi.com/"
+payload = {"json": ""}
 today = date.today().strftime("%m/%d")
-response = requests.get(f"{BASE_URL}{today}/date")
-fact = response.text
 
-# tweet the fact
-resp = twitter.update_status(status=fact)
-print("Tweeted: %s" % resp["text"])
+MAX_CHAR_COUNT = 280
+api_call_count = 0
+fact = "x" * (MAX_CHAR_COUNT+1)
+
+# prevent edge cases where facts are too long
+# or too many calls to the fact API
+while(len(fact) > MAX_CHAR_COUNT and api_call_count < 10):
+
+    response = requests.get(f"{BASE_URL}{today}/date", params=payload)
+    # response == http://numbersapi.com/10/19/date?json=
+
+    data = dict(response.json())
+    fact = data.get("text")
+    api_call_count += 1
+
+# tweet the fact if valid
+if len(fact) <= MAX_CHAR_COUNT:
+    resp = twitter.update_status(status=fact)
+    print("Tweeted: %s" % resp["text"])
+else:
+    # explicit for clarity
+    None
